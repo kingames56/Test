ce66ed470fc0322e66a4c09e146329afff268a89
kingames56 <159518282+kingames56@users.noreply.github.com>
2024-02-09 10:13:51 +0300

    Hi

---
+++ Сравнение вчахпшлвзэдп/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сравнение_вчахпшлвзэдп
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 число");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2 число");
+            int b = Convert.ToInt32(Console.ReadLine());
+       
+            if (a > b) 
+                Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(a);
+            
+            if (b > a)
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(b);
+
+            if (a == b) 
+                Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine(b);
+
+        }
+    }
+}
